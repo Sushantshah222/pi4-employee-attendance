7c232c85619adfe83be52b365df7f3eda9a12e36
sushantshah222 <sushant.shah222@gmail.com>
2025-07-15 16:49:59 +0545

    fix: Refereshing issue fixed

--- start.sh
+++ start.sh
@@ -9,8 +9,11 @@ cd /home/pi0/pi4-employee-attendance
 # Start Django server in background
 python3 manage.py runserver 192.168.1.149:8000 &
 
-# Wait for the server to be ready
+# Wait for Django to be ready
 sleep 10
 
-# Start RFID reader script
-python3 read_keyboard_events.py
+# Start RFID reader script in background too
+python3 read_keyboard_events.py &
+
+# Wait for both background jobs to finish
+wait
